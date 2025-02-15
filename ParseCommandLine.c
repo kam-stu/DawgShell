ed7cd09ea85d9f13629224eca8b8396a621e46d2
GrayLess <165932951+GrayLess@users.noreply.github.com>
2025-02-15 17:57:21 -0600

    urmmm what the sigma

--- ParseCommandLine.c
+++ ParseCommandLine.c
@@ -1,8 +1,42 @@
+#include <string.h>
 
+// making the struct
+struct ShellCommand {
+    char* args;
+    char* fileIn;
+    char* fileOut;
+};
 
 // Process the user input (As a shell command)
 // Note the return type is a ShellCommand struct
 struct ShellCommand ParseCommandLine(char* input);
 
-// teting parsing
+struct ShellCommand ParseCommandLine(char* input) {
+    struct ShellCommand cmd;
+    char* token;
+
+    token = strtok(input, " ");
+    while (token != NULL) {
+        // case 1 (input needs to be redirected)
+        if (strcmp(token, "<") == 0){
+
+            if (token != NULL) {
+                cmd.fileIn = strdup(token);
+            }
+
+        // case 2 (output needs to be redirected)
+        } else if (strcmp(token, ">") == 0) {
+
+            if (token != NULL) {
+                cmd.fileOut = strdup(token);
+            }
+
+        }
+
+    }
+
+    return cmd;
+}
+
+// testing parsing
 int main () {}
